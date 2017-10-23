d8c991589ee376f4464b6273c9b97fb5e934967a
John Krupka <john.r.krupka@gmail.com>
2017-10-23 08:57:06 -0500

    refactor to separate commands

--- CommandHandler.fs
+++ CommandHandler.fs
@@ -4,6 +4,20 @@ open Discord.Commands
 open Discord.WebSocket
 open System.Threading.Tasks
 open Commands
+open Util
+
+let commandPrefix = "//"
+
+let skipPrefix (s: string) =
+    if s.StartsWith(commandPrefix) 
+    then s.Substring(String.length commandPrefix) 
+    else s
+
+let doCommand (client: DiscordSocketClient) (msg: SocketUserMessage) =
+    let msgParts = msg.Content.Split(" ")
+    let cmdName = msgParts |> Array.head |> skipPrefix |> lowerCase
+    let args = Array.tail msgParts
+    (client, msg, args) |||> commands cmdName
 
 let messageRecieved (client: DiscordSocketClient) _ (sm: SocketMessage) = 
     let message = sm :?> SocketUserMessage
--- Commands.fs
+++ Commands.fs
@@ -4,11 +4,10 @@ open Discord.Commands
 open Discord.WebSocket
 open System
 open System.Threading.Tasks
-open Util
 
-let commandPrefix = "//"
+type Handler = DiscordSocketClient -> SocketUserMessage -> string array -> Task
 
-let echo client msg (args: string array) =
+let echo client msg (args: string array)  =
     let context = SocketCommandContext(client, msg)
     let msgOut = String.Join(" ", args)
     context.Channel.SendMessageAsync(msgOut) :> Task
@@ -22,20 +21,9 @@ let unknown client msg _ =
     let context = SocketCommandContext(client, msg)
     context.Channel.SendMessageAsync("Unknown command") :> Task
 
-let commands = 
+let commands: string -> Handler = 
     let cmds =
         [ ("echo", echo)
           ("foo", foo)
         ] |> Map.ofList
     fun cmdName -> Option.defaultValue unknown <| Map.tryFind cmdName cmds
-
-let skipPrefix (s: string) =
-    if s.StartsWith(commandPrefix) 
-    then s.Substring(String.length commandPrefix) 
-    else s
-
-let doCommand (client: DiscordSocketClient) (msg: SocketUserMessage) =
-    let msgParts = msg.Content.Split(" ")
-    let cmdName = msgParts |> Array.head |> skipPrefix |> lowerCase
-    let args = Array.tail msgParts
-    (client, msg, args) |||> commands cmdName
