d4da914598beb63ab23894486a02425990473233
John Krupka <john.r.krupka@gmail.com>
2017-12-07 07:07:12 -0600

    rename EchoPlugin, change to deployment yml

---
+++ DiscordBotFSharp/PluginBundle.fs
@@ -0,0 +1,20 @@
+module PluginBundle
+
+open Plugins
+open Types
+open Commands
+
+module EchoPlugin =
+    let private echo client msg = function
+        | [S s] -> sendMsg client msg (s + " " + s) |> Success
+        | _ -> Fail "echoTwice takes a single string"
+
+    [<NamedCommandAttribute("echoTwice")>]
+    type T() = interface ICommand with member t.Execute = echo
+
+module VersionPlugin =
+    let private version client msg _ = 
+        sendMsg client msg "V1.0.2" |> Success
+
+    [<NamedCommandAttribute("version")>]
+    type T() = interface ICommand with member t.Execute = version
