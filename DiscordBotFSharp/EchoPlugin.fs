7868d1486a1f1f670483fb330c72448a19e8f0d8
John Krupka <john.r.krupka@gmail.com>
2017-12-06 23:15:40 -0600

    add version action to plugin

--- DiscordBotFSharp/EchoPlugin.fs
+++ DiscordBotFSharp/EchoPlugin.fs
@@ -12,3 +12,13 @@ let private doSomething client msg = function
 type T() = 
     interface ICommand with 
         member t.Execute = doSomething
+
+///==========================
+let private version client msg = function
+    | [S s] -> sendMsg client msg (s + " " + s) |> Success
+    | _ -> Fail "V1.0.0"
+
+[<NamedCommandAttribute("version")>]
+type U() = 
+    interface ICommand with 
+        member t.Execute = version
