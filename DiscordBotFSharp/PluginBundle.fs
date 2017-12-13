53433a083ba5ad6a366aab499b7ec947bb2278f3
John Krupka <john.r.krupka@gmail.com>
2017-12-13 08:32:49 -0600

    better logging; catch any exceptions in plugins

--- DiscordBotFSharp/PluginBundle.fs
+++ DiscordBotFSharp/PluginBundle.fs
@@ -4,6 +4,12 @@ open Plugins
 open Types
 open Commands
 open Util
+open System.Threading
+open System.Threading.Tasks
+
+let withFail f client msg args = 
+    try f client msg args 
+    with exn -> exn.Message |> Fail
 
 module EchoPlugin =
     let private echo client msg = function
@@ -11,22 +17,23 @@ module EchoPlugin =
         | _ -> Fail "echoTwice takes a single string"
 
     [<NamedCommandAttribute("echoTwice")>]
-    type T() = interface ICommand with member t.Execute = echo
+    type T() = interface ICommand with member t.Execute = withFail echo
 
 module VersionPlugin =
     let private version client msg _ = 
         version() |> sendMsg client msg |> Success
 
     [<NamedCommandAttribute("version")>]
-    type T() = interface ICommand with member t.Execute = version
+    type T() = interface ICommand with member t.Execute = withFail version
 
 module HelpPlugin =
     let private help client msg _ = 
         botCommands() 
-        |> List.map (fun (k, _) -> k) 
+        |> List.map (fun (k, _) -> k)
+        |> List.sort
         |> sprintf "%A" 
         |> sendMsg client msg 
         |> Success
 
     [<NamedCommandAttribute("help")>]
-    type T() = interface ICommand with member t.Execute = help
+    type T() = interface ICommand with member t.Execute = withFail help
